855a27e9296444e5699059e714cc003ff73dbd5e
mimicao <501377732@qq.com>
2017-10-19 00:18:22 +0800

    Example of function overload

---
+++ day05/FuncDemo1/FuncDemo1.java
@@ -0,0 +1,22 @@
+/*
+方法重载概念
+*/
+class FuncDemo1{
+	public static void main(String[] args){
+		System.out.println(sum(10,20));
+		System.out.println(sum(10,20,30));
+		System.out.println(sum(10.5f, 20));
+	}
+	
+	public static int sum(int a, int b){
+		return (a+b);
+	}
+	
+	public static int sum(int a, int b, int c){
+		return (a+b+c);
+	}
+	
+	public static float sum(float a, float b){
+		return a+b;
+	}
+}
