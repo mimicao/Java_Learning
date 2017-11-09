c384b43dbac820001b380198d6b2ac7a48e853c2
mimicao <501377732@qq.com>
2017-11-09 20:02:29 +0800

    ArrayList

---
+++ JavaSE_workspace/day14_Date/src/cn/laoliu_04/DateFormatDemo.java
@@ -0,0 +1,17 @@
+package cn.laoliu_04;
+
+import java.text.ParseException;
+import java.util.Date;
+
+public class DateFormatDemo {
+	public static void main(String[] args) throws ParseException {
+		Date d = new Date();
+
+		String s = DateUtil.dateToString(d, "yyyyÄêMMÔÂddÈÕ");
+		System.out.println(s);
+
+		String s1 = "2015-12-25";
+		Date d1 = DateUtil.stringToDate(s1, "yyyy-MM-dd");
+		System.out.println(d1);
+	}
+}
---
+++ JavaSE_workspace/day15_Collection/src/cn/laoliu_02/CollectionDemo.java
@@ -0,0 +1,34 @@
+package cn.laoliu_02;
+
+/*
+ * boolean addAll(Collection c)
+ * boolean removeAll(Collection c)
+ * boolean containsAll(Collection c)
+ * boolean retainAll(Collection c)
+ */
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CollectionDemo {
+	public static void main(String[] args) {
+		Collection c1 = new ArrayList();
+		c1.add("abc1");
+		c1.add("abc2");
+		c1.add("abc3");
+		c1.add("abc4");
+
+		Collection c2 = new ArrayList();
+		c2.add("abc4");
+		c2.add("abc5");
+		c2.add("abc6");
+		c2.add("abc7");
+
+		// System.out.println("addAll:"+c1.addAll(c2));
+		// System.out.println("removeAll:"+c1.removeAll(c2));
+		// System.out.println("containsAll:"+c1.containsAll(c2));
+		System.out.println("retainAll£º" + c1.retainAll(c2));
+
+		System.out.println("c1:" + c1);
+		System.out.println("c2:" + c2);
+	}
+}
